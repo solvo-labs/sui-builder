a496465e0f046e997860d00e77506913b959d666
afarukcali <afarukcali@gmail.com>
2023-12-27 13:03:39 +0300

    nft sample request.

--- nft.js
+++ nft.js
@@ -45,10 +45,24 @@ const init = async () => {
   //   ],
   // });
 
+  // mint
+  // tx.moveCall({
+  //   target: "0x44d12155bb085df7d5432f0ad2419eb46195c449c327c716f43b733cfd17884d::devnet_nft::mint_to_sender",
+  //   arguments: [tx.pure.string("yakya"), tx.pure.string("yaa"), tx.pure.string("https://ipfs.io/ipfs/bafkreidhxli7jw5swgbjvjdbn5gxehpfrduq22laamlrkakpvxxqivlpi4")],
+  //   typeArguments: [],
+  // });
+
+  // transfer
+  // tx.moveCall({
+  //   target: "0x44d12155bb085df7d5432f0ad2419eb46195c449c327c716f43b733cfd17884d::devnet_nft::transfer",
+  //   arguments: [tx.object("0x049349dc7dbf86ddfdf541431fb2c1948e902c854f1b3e39eb4104c2f6f8c5ef"), tx.pure("0xc0080d8cbbe6e1b9b44652b4649762548d50a6f327a882f21bd47ef4c88d2c5d")],
+  // });
+
+  // 0x48e3b9654f77bf376698de702f7bf89d68d0b035dc4e3bf7d7d234d35882b72b;
+
   tx.moveCall({
-    target: "0x44d12155bb085df7d5432f0ad2419eb46195c449c327c716f43b733cfd17884d::devnet_nft::mint_to_sender",
-    arguments: [tx.pure.string("SYM"), tx.pure.string("name"), tx.pure.string("https://ipfs.io/ipfs/bafkreidhxli7jw5swgbjvjdbn5gxehpfrduq22laamlrkakpvxxqivlpi4")],
-    typeArguments: [],
+    target: "0x44d12155bb085df7d5432f0ad2419eb46195c449c327c716f43b733cfd17884d::devnet_nft::burn",
+    arguments: [tx.object("0x650ea7aa0cfc27dbe196e614f2e3105396e2e73e39480dd74ef9cde8be86b599")],
   });
 
   try {
